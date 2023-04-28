f8ea8248c2561217cb51c669bc7919a92dccbcd4
Walid Khaddoum <walidkhaddoum@gmail.com>
2023-04-28 04:56:38 -0700

    Fix Whole project

--- builtins.c
+++ builtins.c
@@ -1,152 +1,97 @@
 #include "shell.h"
 
-int shellby_alias(char **args, char __attribute__((__unused__)) **front);
-void set_alias(char *var_name, char *value);
-void print_alias(alias_t *alias);
+/**
+ * _myexit - exits the shell
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: exits with a given exit status
+ *         (0) if info.argv[0] != "exit"
+ */
+int _myexit(info_t *info)
+{
+	int exitcheck;
+
+	if (info->argv[1])  /* If there is an exit arguement */
+	{
+		exitcheck = _erratoi(info->argv[1]);
+		if (exitcheck == -1)
+		{
+			info->status = 2;
+			print_error(info, "Illegal number: ");
+			_eputs(info->argv[1]);
+			_eputchar('\n');
+			return (1);
+		}
+		info->err_num = _erratoi(info->argv[1]);
+		return (-2);
+	}
+	info->err_num = -1;
+	return (-2);
+}
 
 /**
- * shellby_alias - Builtin command that either prints all aliases, specific
- * aliases, or sets an alias.
- * @args: An array of arguments.
- * @front: A double pointer to the beginning of args.
- *
- * Return: If an error occurs - -1.
- *         Otherwise - 0.
+ * _mycd - changes the current directory of the process
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
  */
-int shellby_alias(char **args, char __attribute__((__unused__)) **front)
+int _mycd(info_t *info)
 {
-	alias_t *temp = aliases;
-	int i, ret = 0;
-	char *value;
+	char *s, *dir, buffer[1024];
+	int chdir_ret;
 
-	if (!args[0])
+	s = getcwd(buffer, 1024);
+	if (!s)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+	if (!info->argv[1])
 	{
-		while (temp)
-		{
-			print_alias(temp);
-			temp = temp->next;
-		}
-		return (ret);
-	}
-	for (i = 0; args[i]; i++)
-	{
-		temp = aliases;
-		value = _strchr(args[i], '=');
-		if (!value)
-		{
-			while (temp)
-			{
-				if (_strcmp(args[i], temp->name) == 0)
-				{
-					print_alias(temp);
-					break;
-				}
-				temp = temp->next;
-			}
-			if (!temp)
-				ret = create_error(args + i, 1);
-		}
+		dir = _getenv(info, "HOME=");
+		if (!dir)
+			chdir_ret = /* TODO: what should this be? */
+				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
 		else
-			set_alias(args[i], value);
+			chdir_ret = chdir(dir);
 	}
-	return (ret);
-}
-
-/**
- * set_alias - Will either set an existing alias 'name' with a new value,
- * 'value' or creates a new alias with 'name' and 'value'.
- * @var_name: Name of the alias.
- * @value: Value of the alias. First character is a '='.
- */
-void set_alias(char *var_name, char *value)
-{
-	alias_t *temp = aliases;
-	int len, j, k;
-	char *new_value;
-
-	*value = '\0';
-	value++;
-	len = _strlen(value) - _strspn(value, "'\"");
-	new_value = malloc(sizeof(char) * (len + 1));
-	if (!new_value)
-		return;
-	for (j = 0, k = 0; value[j]; j++)
+	else if (_strcmp(info->argv[1], "-") == 0)
 	{
-		if (value[j] != '\'' && value[j] != '"')
-			new_value[k++] = value[j];
-	}
-	new_value[k] = '\0';
-	while (temp)
-	{
-		if (_strcmp(var_name, temp->name) == 0)
+		if (!_getenv(info, "OLDPWD="))
 		{
-			free(temp->value);
-			temp->value = new_value;
-			break;
+			_puts(s);
+			_putchar('\n');
+			return (1);
 		}
-		temp = temp->next;
+		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+		chdir_ret = /* TODO: what should this be? */
+			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
 	}
-	if (!temp)
-		add_alias_end(&aliases, var_name, new_value);
-}
-
-/**
- * print_alias - Prints the alias in the format name='value'.
- * @alias: Pointer to an alias.
- */
-void print_alias(alias_t *alias)
-{
-	char *alias_string;
-	int len = _strlen(alias->name) + _strlen(alias->value) + 4;
-
-	alias_string = malloc(sizeof(char) * (len + 1));
-	if (!alias_string)
-		return;
-	_strcpy(alias_string, alias->name);
-	_strcat(alias_string, "='");
-	_strcat(alias_string, alias->value);
-	_strcat(alias_string, "'\n");
-
-	write(STDOUT_FILENO, alias_string, len);
-	free(alias_string);
-}
-/**
- * replace_aliases - Goes through the arguments and replace any matching alias
- * with their value.
- * @args: 2D pointer to the arguments.
- *
- * Return: 2D pointer to the arguments.
- */
-char **replace_aliases(char **args)
-{
-	alias_t *temp;
-	int i;
-	char *new_value;
-
-	if (_strcmp(args[0], "alias") == 0)
-		return (args);
-	for (i = 0; args[i]; i++)
+	else
+		chdir_ret = chdir(info->argv[1]);
+	if (chdir_ret == -1)
 	{
-		temp = aliases;
-		while (temp)
-		{
-			if (_strcmp(args[i], temp->name) == 0)
-			{
-				new_value = malloc(sizeof(char) * (_strlen(temp->value) + 1));
-				if (!new_value)
-				{
-					free_args(args, args);
-					return (NULL);
-				}
-				_strcpy(new_value, temp->value);
-				free(args[i]);
-				args[i] = new_value;
-				i--;
-				break;
-			}
-			temp = temp->next;
-		}
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]), _eputchar('\n');
 	}
+	else
+	{
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
+}
 
-	return (args);
+/**
+ * _myhelp - changes the current directory of the process
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
+ */
+int _myhelp(info_t *info)
+{
+	char **arg_array;
+
+	arg_array = info->argv;
+	_puts("help call works. Function not yet implemented \n");
+	if (0)
+		_puts(*arg_array); /* temp att_unused workaround */
+	return (0);
 }
